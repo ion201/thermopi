f22c3e46ec8c7cf72f7f68a4a9aee36555c052f4
ion201 <moniseat101@yahoo.com>
2015-02-03 02:45:41 +0000

    Update with GPIO

--- IO.py
+++ IO.py
@@ -1,6 +1,17 @@
 import RPi.GPIO as GPIO
 
 
+# GPIO.output(18, 0)  # Turn fan on
+# GPIO.output(18, 1)  # Turn fan off
+# GPIO.output(16, 0)  # Turn AC on
+# GPIO.output(16, 1)  # Turn AC off
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(18, GPIO.OUT, initial=1)
+    GPIO.setup(16, GPIO.OUT, initial=1)
+
+
 def gettemp():
     return 99
 
@@ -8,7 +19,7 @@ def gettemp():
 def setfan(state):
     pass
 
-   
+
 def setac(state):
     if state:
         # Always turn on the fan when the ac is on
