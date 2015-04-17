c583bcf6ae2afb8d81770eb8e72abf764da930e0
ion201 <moniseat101@yahoo.com>
2015-04-17 13:57:45 -0700

    add installer content

--- install.sh
+++ install.sh
@@ -8,14 +8,27 @@ fi
 
 if [ `pwd` != '/srv/thermopi' ]
 then
-    # Copy the files
-    cp -r ../thermopi /srv/
+    echo -n 'Copying files to /srv... '
+    rsync -rog ../thermopi /srv/
     cd /srv/thermopi
+    echo 'Done!'
 fi
 
 if [ `pidof systemd` ]
 then
     # Run installer for systemd components
+    echo -n 'Installing and starting systemd service... '
+    cp thermopiIO.service /etc/systemd/system/
+    systemctl enable thermopiIO
+    systemctl start thermopiIO
+    echo 'Done!'
 else
     # Let's assume if it's not systemd it will use /etc/init.d
+    echo 'You should be using systemd...'
+    echo -n 'Installing and starting service thermopiIO... '
+    cp thermopiIO.sh /etc/init.d/
+    service thermopiIO start
+    echo 'Done!'
 fi
+
+# TODO: instructions for setting apache settings
